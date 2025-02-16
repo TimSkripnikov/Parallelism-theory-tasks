1120c64151fef2b181d4bf48d49a32294426c8ef
Artem <art.skripnikov@gmail.com>
2025-02-16 12:30:16 +0000

    Initial commit

---
+++ task1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#ifndef USE_DOUBLE
+    typedef float invariant_t;
+#else
+    typedef double invariant_t;
+#endif
+
+const size_t n = 10000000;
+
+int main(){
+    std::vector<invariant_t> data(n);
+
+    for (size_t i = 0; i<n; ++i) {data[i] = std::sin((2*M_PI*i)/n);}
+    
+
+    invariant_t sum = 0;
+    for (size_t i = 0; i<n; ++i) {sum += data[i];}
+
+    std::cout << "Sum: " << sum << std::endl;
+
+}
